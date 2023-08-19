c655af3a8d6fc0dd1a487d345c912559dbbe7076
viniciusmarangoni <viniciusmarangoni@users.noreply.github.com>
2023-08-19 16:15:28 -0300

    use bash as login shell to start tilix

--- start-new-window.sh
+++ start-new-window.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tilix --geometry=230x50+50+50 -e "/bin/bash -c \"gdb -q $@ ; echo exited ; read\""
+tilix --geometry=230x50+50+50 -e "/bin/bash -l -c \"/usr/bin/gdb -q $@ ; echo exited ; read\""
